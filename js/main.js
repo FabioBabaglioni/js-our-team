7cdb08cae4986d655227d825ef686166b2131271
FabioBabaglioni <fabiobabaglioni@gmail.com>
2022-11-21 15:34:07 +0100

    inerimento dati e primo console.log

---
+++ js/main.js
@@ -0,0 +1,56 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+const Team = [
+
+    {
+        "name" : "Wayne Barnett",
+        "ruolo" : "Founder e CEO",
+        "foto" : "img/Wayne-Barnett-founder-ceo.jpg"
+    } ,
+
+    {
+        "name" : "Angela Carol",
+        "ruolo" : "Chief editor",
+        "foto" : "img/angela-caroll-chief-editor.jpg"
+    },
+
+    {
+        "name" : "Walter Gordon",
+        "ruolo" : "Office Manager",
+        "foto" : "img/Walter-Gordon-office-manager.jpg"
+    },
+
+    {
+        "name" : "Angela Lopez",
+        "ruolo" : "Social Media Manager",
+        "foto" : "img/angela-lopez-social-media-manager.jpg"
+    },
+
+    {
+        "name" : "Scott Extrada",
+        "ruolo" : "Developer",
+        "foto" : "img/scott-extrada-social-developer.jpg"
+    },
+
+    {
+        "name" : "Barbara Ramos",
+        "ruolo" : "Graphic Designer",
+        "foto" : "img/barbara-ramos-graphic-designer.jpg"
+    },
+]
+
+console.log(Team);
+
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+
+
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
